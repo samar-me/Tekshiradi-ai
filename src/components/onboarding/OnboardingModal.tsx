9083eb620eab9a4fe34546f2f4a8f9b3fad00c8a
samar-me <samar@users.noreply.github.com>
2026-08-25 05:29:40 -0700

    feat: add all Uzbekistan districts and cities

--- src/components/onboarding/OnboardingModal.tsx
+++ src/components/onboarding/OnboardingModal.tsx
@@ -4,20 +4,20 @@ import { ArrowLeft, ArrowRight, Mail, Phone, ScanLine, ShieldCheck } from 'lucid
 import { useTelegram } from '../providers/TelegramProvider';
 
 const regions = [
-  ['Toshkent shahri','Bektemir,Chilonzor,Mirobod,Mirzo Ulug‘bek,Olmazor,Sergeli,Shayxontohur,Uchtepa,Yakkasaroy,Yashnobod,Yunusobod'],
-  ['Toshkent viloyati','Angren,Bekobod,Bo‘stonliq,Chirchiq,Qibray,Olmaliq,Parkent,Yangiyo‘l'],
-  ['Andijon viloyati','Andijon shahri,Asaka,Baliqchi,Marhamat,Shahrixon'],
-  ['Buxoro viloyati','Buxoro shahri,G‘ijduvon,Kogon,Romitan,Vobkent'],
-  ['Farg‘ona viloyati','Farg‘ona shahri,Qo‘qon,Marg‘ilon,Quva,Rishton'],
-  ['Jizzax viloyati','Jizzax shahri,G‘allaorol,Zomin,Paxtakor'],
-  ['Namangan viloyati','Namangan shahri,Chortoq,Chust,Pop,Uychi'],
-  ['Navoiy viloyati','Navoiy shahri,Zarafshon,Karmana,Xatirchi'],
-  ['Qashqadaryo viloyati','Qarshi shahri,Shahrisabz,Kitob,Koson,Muborak'],
-  ['Qoraqalpog‘iston Respublikasi','Nukus shahri,Beruniy,Chimboy,Qo‘ng‘irot,To‘rtko‘l'],
-  ['Samarqand viloyati','Samarqand shahri,Kattaqo‘rg‘on,Bulung‘ur,Jomboy,Urgut'],
-  ['Sirdaryo viloyati','Guliston shahri,Boyovut,Sardoba,Sirdaryo'],
-  ['Surxondaryo viloyati','Termiz shahri,Denov,Jarqo‘rg‘on,Sherobod'],
-  ['Xorazm viloyati','Urganch shahri,Xiva,Hazorasp,Shovot'],
+  ['Toshkent shahri','Bektemir tumani,Chilonzor tumani,Mirobod tumani,Mirzo Ulug‘bek tumani,Olmazor tumani,Sergeli tumani,Shayxontohur tumani,Uchtepa tumani,Yakkasaroy tumani,Yangihayot tumani,Yashnobod tumani,Yunusobod tumani'],
+  ['Toshkent viloyati','Angren shahri,Bekobod shahri,Chirchiq shahri,Nurafshon shahri,Ohangaron shahri,Olmaliq shahri,Yangiyo‘l shahri,Bekobod tumani,Bo‘ka tumani,Bo‘stonliq tumani,Chinoz tumani,Ohangaron tumani,Oqqo‘rg‘on tumani,Parkent tumani,Piskent tumani,Qibray tumani,Quyi Chirchiq tumani,Toshkent tumani,Yangiyo‘l tumani,Yuqori Chirchiq tumani,Zangiota tumani'],
+  ['Andijon viloyati','Andijon shahri,Xonobod shahri,Andijon tumani,Asaka tumani,Baliqchi tumani,Bo‘ston tumani,Buloqboshi tumani,Izboskan tumani,Jalaquduq tumani,Marhamat tumani,Oltinko‘l tumani,Paxtaobod tumani,Qo‘rg‘ontepa tumani,Shahrixon tumani,Ulug‘nor tumani,Xo‘jaobod tumani'],
+  ['Buxoro viloyati','Buxoro shahri,Kogon shahri,Buxoro tumani,G‘ijduvon tumani,Jondor tumani,Kogon tumani,Olot tumani,Peshku tumani,Qorako‘l tumani,Qorovulbozor tumani,Romitan tumani,Shofirkon tumani,Vobkent tumani'],
+  ['Farg‘ona viloyati','Farg‘ona shahri,Qo‘qon shahri,Marg‘ilon shahri,Quvasoy shahri,Beshariq tumani,Bog‘dod tumani,Buvayda tumani,Dang‘ara tumani,Furqat tumani,Oltiariq tumani,O‘zbekiston tumani,Qo‘shtepa tumani,Quva tumani,Rishton tumani,So‘x tumani,Toshloq tumani,Uchko‘prik tumani,Yozyovon tumani'],
+  ['Jizzax viloyati','Jizzax shahri,Arnasoy tumani,Baxmal tumani,Do‘stlik tumani,Forish tumani,G‘allaorol tumani,Jizzax tumani,Mirzacho‘l tumani,Paxtakor tumani,Sharof Rashidov tumani,Yangiobod tumani,Zafarobod tumani,Zarbdor tumani,Zomin tumani'],
+  ['Namangan viloyati','Namangan shahri,Chortoq tumani,Chust tumani,Kosonsoy tumani,Mingbuloq tumani,Namangan tumani,Norin tumani,Pop tumani,To‘raqo‘rg‘on tumani,Uchqo‘rg‘on tumani,Uychi tumani,Yangiqo‘rg‘on tumani'],
+  ['Navoiy viloyati','Navoiy shahri,Zarafshon shahri,G‘ozg‘on shahri,Karmana tumani,Konimex tumani,Navbahor tumani,Nurota tumani,Qiziltepa tumani,Tomdi tumani,Uchquduq tumani,Xatirchi tumani'],
+  ['Qashqadaryo viloyati','Qarshi shahri,Shahrisabz shahri,Chiroqchi tumani,Dehqonobod tumani,G‘uzor tumani,Kasbi tumani,Kitob tumani,Koson tumani,Ko‘kdala tumani,Mirishkor tumani,Muborak tumani,Nishon tumani,Qamashi tumani,Qarshi tumani,Shahrisabz tumani,Yakkabog‘ tumani'],
+  ['Qoraqalpog‘iston Respublikasi','Nukus shahri,Amudaryo tumani,Beruniy tumani,Bo‘zatov tumani,Chimboy tumani,Ellikqal‘a tumani,Kegeyli tumani,Mo‘ynoq tumani,Nukus tumani,Qanliko‘l tumani,Qo‘ng‘irot tumani,Qorao‘zak tumani,Shumanay tumani,Taxtako‘pir tumani,Taxiatosh tumani,To‘rtko‘l tumani,Xo‘jayli tumani'],
+  ['Samarqand viloyati','Samarqand shahri,Kattaqo‘rg‘on shahri,Bulung‘ur tumani,Ishtixon tumani,Jomboy tumani,Kattaqo‘rg‘on tumani,Narpay tumani,Nurobod tumani,Oqdaryo tumani,Paxtachi tumani,Pastdarg‘om tumani,Payariq tumani,Qo‘shrabot tumani,Samarqand tumani,Toyloq tumani,Urgut tumani'],
+  ['Sirdaryo viloyati','Guliston shahri,Shirin shahri,Yangiyer shahri,Boyovut tumani,Guliston tumani,Mirzaobod tumani,Oqoltin tumani,Sardoba tumani,Sayxunobod tumani,Sirdaryo tumani,Xovos tumani'],
+  ['Surxondaryo viloyati','Termiz shahri,Angor tumani,Bandixon tumani,Boysun tumani,Denov tumani,Jarqo‘rg‘on tumani,Muzrabot tumani,Oltinsoy tumani,Qiziriq tumani,Qumqo‘rg‘on tumani,Sariosiyo tumani,Sherobod tumani,Sho‘rchi tumani,Termiz tumani,Uzun tumani'],
+  ['Xorazm viloyati','Urganch shahri,Xiva shahri,Bog‘ot tumani,Gurlan tumani,Hazorasp tumani,Xiva tumani,Xonqa tumani,Qo‘shko‘pir tumani,Shovot tumani,Tuproqqal‘a tumani,Urganch tumani,Yangiariq tumani,Yangibozor tumani'],
 ] as const;
 const subjects=['Matematika','Ona tili','Adabiyot','Ingliz tili','Rus tili','Informatika','Fizika','Kimyo','Biologiya','Tarix','Geografiya','Boshlang‘ich ta’lim','Boshqa'];
 
